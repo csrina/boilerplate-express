061e4d6603e3acf3c09af0da72c40656e5636507
Virina Mekhail <virnamekhail@hotmail.com>
2021-05-07 12:06:33 -0600

    Server index.html to the browser

--- myApp.js
+++ myApp.js
@@ -5,7 +5,10 @@ var app = express();
 console.log("Hello World");
 
 //2. Start a Working Express Server
-app.get("/", (req, res) => res.send("Hello Express"));
+app.get("/hello-express", (_, res) => res.send("Hello Express"));
+
+//3. Serve an HTML File
+app.get("/", (_, res) => res.sendFile(__dirname+"/views/index.html"));
 
 
 module.exports = app;
