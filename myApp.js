0d123ae47e8eb5306e0ef8e36ae01b20a95068e9
Virina Mekhail <virnamekhail@hotmail.com>
2021-05-07 12:23:05 -0600

    Server pulbic folder with static files to the browser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+//4. serve static files
+app.use('/public', express.static(__dirname + '/public'));
+
 // 1. Meet the Node consolePassed
 console.log("Hello World");
 
@@ -8,7 +11,7 @@ console.log("Hello World");
 app.get("/hello-express", (_, res) => res.send("Hello Express"));
 
 //3. Serve an HTML File
-app.get("/", (_, res) => res.sendFile(__dirname+"/views/index.html"));
+app.get("/", (_, res) => res.sendFile(__dirname + "/views/index.html"));
 
 
 module.exports = app;
