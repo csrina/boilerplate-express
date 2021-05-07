14e8fce7d6d65f3823803011c5f47079342fba8f
Virina Mekhail <virnamekhail@hotmail.com>
2021-05-07 13:20:16 -0600

    use the env file, and add dotenv as a dep

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 //4. serve static files
 app.use('/public', express.static(__dirname + '/public'));
@@ -14,6 +15,13 @@ app.get("/hello-express", (_, res) => res.send("Hello Express"));
 app.get("/", (_, res) => res.sendFile(__dirname + "/views/index.html"));
 
 //5. Serve JSON on a Specific Route
-app.get("/json", (_, res) => res.json({"message": "Hello json"}));
+app.get("/json-v1", (_, res) => res.json({"message": "Hello json"}));
+
+//6. Use the env file
+app.get("/json", (_, res) => {
+    var hw = "Hello json";
+    console.log(process.env.MESSAGE_STYLE);
+    res.json({"message": (process.env.MESSAGE_STYLE === "uppercase")? hw.toUpperCase() : hw})
+});
 
 module.exports = app;
