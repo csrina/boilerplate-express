3d4126fa06c7db23e37309d9592706d8899f27be
Virina Mekhail <virnamekhail@hotmail.com>
2021-05-07 12:00:04 -0600

    print hello express to browser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
-console.log('Hello World');
+// 1. Meet the Node consolePassed
+console.log("Hello World");
+
+//2. Start a Working Express Server
+app.get("/", (req, res) => res.send("Hello Express"));
+
 
 module.exports = app;
